2576e34fbb092463ecc00cd5ec753015ee75f625
Sarthak Sharma <sarthaksharma2199@outlook.com>
2020-07-30 18:52:14 +0530

    basic merge test

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import PyPDF2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+merger=PyPDF2.PdfFileMerger()
+input1 = open("PDF_Samples/AutoCad_Diagram.pdf", "rb")
+input2 = open("PDF_Samples/AutoCad_Diagram.pdf", "rb")
+input3 = open("PDF_Samples/AutoCad_Diagram.pdf", "rb")
 
+merger.append(input1)
+merger.append(input2)
+merger.append(input3)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Write to an output PDF document
+output = open("document-output.pdf", "wb")
+merger.write(output)
