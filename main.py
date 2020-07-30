92c4265703228299c1ac002806a3aacab43d85ad
Sarthak Sharma <sarthaksharma2199@outlook.com>
2020-07-30 20:22:23 +0530

    added input sample

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import PyPDF2
+import argparse
 
-merger=PyPDF2.PdfFileMerger()
+merger = PyPDF2.PdfFileMerger()
+parser = argparse.ArgumentParser(description="Input of pdfs to merge")
+
 input1 = open("PDF_Samples/AutoCad_Diagram.pdf", "rb")
 input2 = open("PDF_Samples/AutoCad_Diagram.pdf", "rb")
 input3 = open("PDF_Samples/AutoCad_Diagram.pdf", "rb")
@@ -9,6 +12,9 @@ merger.append(input1)
 merger.append(input2)
 merger.append(input3)
 
-# Write to an output PDF document
-output = open("document-output.pdf", "wb")
-merger.write(output)
+def write_final_pdf(output_file_name="output.pdf"):
+    output = open(output_file_name, "wb")
+    merger.write(output)
+
+
+write_final_pdf("out.pdf")
